839db512cf8078c57847b92f5d8b3104fc795b9b
yoojae <lea_25@naver,com>
2023-04-12 18:52:41 +0900

    요청사항 수정 완료

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -41,9 +41,10 @@ class Introduce extends StatelessWidget {
       children: [
         Container(height: 200.h,),
         Text('BIT의 솔루션', style: TextStyle(
-          fontSize: 40.sp,
+          fontSize: 43.sp,
         ),
         ),
+        Container(height: 30.h,),
         Text('모발과 두피 탈모 케어를 초월한 뷰티 브레인 테라피', style: TextStyle(
           fontSize: 30.sp,
         ),
@@ -52,216 +53,22 @@ class Introduce extends StatelessWidget {
           fontSize: 30.sp,
         ),
         ),
-        Container(height: 70.h,),
+        Container(height: 90.h,),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              children: [
-                Image.asset('images/profile01.jpg',
-                  width: 450.w,
-                  height: 850.h,
-                  fit: BoxFit.cover,
-                ),
-                Container(height: 30.h,),
-                Text('두피 탈모 전문가',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 20.sp,
-                  ),
-                ),
-                Container(height: 5.h,),
-                RichText(
-                  text: TextSpan(
-                      text: '김혜진',
-                      style: TextStyle(
-                        fontSize: 35.sp,
-                        fontWeight: FontWeight.w300,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: ' 원장',
-                          style: TextStyle(
-                            fontSize: 30.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-              ],
-            ),
-            Container(width: 50.w,),
-            Column(
-              children: [
-                Image.asset('images/profile02.jpg',
-                  width: 450.w,
-                  height: 850.h,
-                  fit: BoxFit.cover,
-                ),
-                Container(height: 30.h,),
-                Text('두피 탈모 전문가',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 20.sp,
-                  ),
-                ),
-                Container(height: 5.h,),
-                RichText(
-                  text: TextSpan(
-                      text: '이서연',
-                      style: TextStyle(
-                        fontSize: 35.sp,
-                        fontWeight: FontWeight.w300,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: ' 실장',
-                          style: TextStyle(
-                            fontSize: 30.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-              ],
-            ),
+            IntroDesigner(sub: '두피 탈모 전문가', name: '김혜진', img: 'images/profile01.jpg', rank: ' 원장'),
+            Container(width: 60.w,),
+            IntroDesigner(sub: '두피 탈모 전문가', name: '이서연', img: 'images/profile02.jpg', rank: ' 실장'),
           ],
         ),
         Container(height: 200.h,),
         Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                padding: EdgeInsets.all(40.0.sp),
-                width: 700.w,
-                height: 500.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 15,
-                      offset: const Offset(10, 10),
-                      color: Colors.black.withOpacity(0.1),
-                    )
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 700.w,
-                      height: 100.h,
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Color(0xffcec3b8),
-                            width: 3.0,
-                          ),
-                        ),
-                      ),
-                      child: Text('두피 스켈링', style: TextStyle(
-                        fontSize: 30.sp,
-                        fontWeight: FontWeight.w800,
-                      ),),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 30.h),
-                      child: Column(
-                        children: [
-                          Text('두피속 노폐물을 친환경 에센셜 오일을 활용해 제거하고 두피 혈액순환을 도와 두피 질환과 탈모를 예방하는 아로마 테라피'),
-                          Container(height: 100.h,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('80,000(30m)', style: TextStyle(
-                                fontSize: 27.sp,
-                                fontWeight: FontWeight.w400,
-                              ),),
-                              Container(width: 25.w,),
-                              Text('150,000(60m)', style: TextStyle(
-                                fontSize: 27.sp,
-                                fontWeight: FontWeight.w400,
-                              ),),
-                              Container(width: 35.w,),
-                              Text('회원권 10회+2회', style: TextStyle(
-                                fontSize: 22.sp,
-                                fontWeight: FontWeight.w400,
-                              ),),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              Process(title: '두피 스켈링', sub: '두피속 노폐물을 친환경 에센셜 오일을 활용해 제거하고 두피 혈액순환을 도와 두피 질환과 탈모를 예방하는 아로마 테라피',),
               Container(width: 100.w,),
-              Container(
-                padding: EdgeInsets.all(40.0.sp),
-                width: 700.w,
-                height: 500.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 15,
-                      offset: const Offset(10, 10),
-                      color: Colors.black.withOpacity(0.1),
-                    )
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 700.w,
-                      height: 100.h,
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Color(0xffcec3b8),
-                            width: 3.0,
-                          ),
-                        ),
-                      ),
-                      child: Text('두피 개선 관리', style: TextStyle(
-                        fontSize: 30.sp,
-                        fontWeight: FontWeight.w800,
-                      ),),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 30.h),
-                      child: Column(
-                        children: [
-                          Text('모발이 자라는 두피환경을 개선하여 탈모 중지와 두피개선을 우선으로하여 영양관리를 진행합니다'),
-                          Container(height: 100.h,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('120,000(40m)', style: TextStyle(
-                                fontSize: 27.sp,
-                                fontWeight: FontWeight.w400,
-                              ),),
-                              Container(width: 25.w,),
-                              Text('150,000(60m)', style: TextStyle(
-                                fontSize: 27.sp,
-                                fontWeight: FontWeight.w400,
-                              ),),
-                              Container(width: 35.w,),
-                              Text('회원권 10회+2회', style: TextStyle(
-                                fontSize: 22.sp,
-                                fontWeight: FontWeight.w400,
-                              ),),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              Process(title: '두피 개선 관리', sub: '모발이 자라는 두피환경을 개선하여 탈모 중지와 두피개선을 우선으로하여 영양관리를 진행합니다',),
             ]
         ),
         Container(height: 200.h,),
@@ -269,9 +76,8 @@ class Introduce extends StatelessWidget {
           padding: EdgeInsets.all(40.0.sp),
           margin: EdgeInsets.only(bottom: 300.h),
           width: 1000.w,
-          height: 700.w,
+          height: 450.w,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
             color: Colors.white,
             boxShadow: [
               BoxShadow(
@@ -286,7 +92,8 @@ class Introduce extends StatelessWidget {
             children: [
               Container(
                 width: 1000.w,
-                height: 100.h,
+                height: 120.h,
+                padding: EdgeInsets.only(bottom: 40.h,),
                 decoration: BoxDecoration(
                   border: Border(
                     bottom: BorderSide(
@@ -295,8 +102,9 @@ class Introduce extends StatelessWidget {
                     ),
                   ),
                 ),
-                child: Center(
-                  child: Text('탈모 약정관리 SPECiAL AGREEMENT CARE', style: TextStyle(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('탈모 약정관리   Special Agreement Care', style: TextStyle(
                     fontSize: 30.sp,
                     fontWeight: FontWeight.w800,
                   ),),
@@ -307,51 +115,16 @@ class Introduce extends StatelessWidget {
                 child: Column(
                   children: [
                     Container(height: 30.h,),
-                    Text('약정 관리 90min 360,000원', style: TextStyle(
-                      fontSize: 28.sp,
+                    Text('약정 관리 90분 __ 360,000원', style: TextStyle(
+                      fontSize: 27.sp,
                     ),),
-                    Text('월간 10명 한정 선착순 할인', style: TextStyle(
-                      fontSize: 28.sp,
+                    Text('월간 10명을 한정해 선착순 할인합니다', style: TextStyle(
+                      fontSize: 27.sp,
                     ),),
-                    Container(height: 0.h,),
+                    Container(height: 50.h,),
                     Text('대상: 두피 진단, 상담 후 결정', style: TextStyle(
-                      fontSize: 28.sp,
+                      fontSize: 27.sp,
                     ),),
-                    Text('계약 조건', style: TextStyle(
-                      fontSize: 28.sp,
-                    ),),
-                    Text('- 불만족시 100% 환불', style: TextStyle(
-                      fontSize: 28.sp,
-                    ),),
-                    Text('- 주1회 이상 총 24회 관리 조건', style: TextStyle(
-                      fontSize: 28.sp,
-                    ),),
-                    Text('- 홈케어 관리 매일 사용조건', style: TextStyle(
-                      fontSize: 28.sp,
-                    ),),
-                    Text('- 기타 약정 내용에 따름', style: TextStyle(
-                      fontSize: 28.sp,
-                    ),),
-                    Container(height: 90.h,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('8,640,000에서', style: TextStyle(
-                          fontSize: 27.sp,
-                          fontWeight: FontWeight.w400,
-                        ),),
-                        Container(width: 25.w,),
-                        Text('5,500,000 선착할인', style: TextStyle(
-                          fontSize: 27.sp,
-                          fontWeight: FontWeight.w400,
-                        ),),
-                        Container(width: 35.w,),
-                        Text('24회 약정관리에 한함', style: TextStyle(
-                          fontSize: 22.sp,
-                          fontWeight: FontWeight.w400,
-                        ),),
-                      ],
-                    ),
                   ],
                 ),
               ),
@@ -359,6 +132,112 @@ class Introduce extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+
+class IntroDesigner extends StatelessWidget {
+  const IntroDesigner({Key? key, required this.sub, required this.name, required this.img, required this.rank}) : super(key: key);
+
+  final String sub, name, rank;
+  final img;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(img,
+          width: 450.w,
+          height: 850.h,
+          fit: BoxFit.cover,
+        ),
+        Container(height: 45.h,),
+        Text(sub,
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 23.sp,
+          ),
+        ),
+        Container(height: 10.h,),
+        RichText(
+          text: TextSpan(
+              text: name,
+              style: TextStyle(
+                fontSize: 35.sp,
+                fontWeight: FontWeight.w300,
+              ),
+              children: [
+                TextSpan(
+                  text: rank,
+                  style: TextStyle(
+                    fontSize: 30.sp,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ]
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+class Process extends StatelessWidget {
+  const Process({Key? key, required this.title, required this.sub}) : super(key: key);
+
+  final String title,sub;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(40.0.sp),
+      width: 700.w,
+      height: 400.h,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 15,
+            offset: const Offset(10, 10),
+            color: Colors.black.withOpacity(0.1),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: 700.w,
+            height: 100.h,
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Color(0xffcec3b8),
+                  width: 3.0,
+                ),
+              ),
+            ),
+            child: Text(title, style: TextStyle(
+              fontSize: 30.sp,
+              fontWeight: FontWeight.w800,
+            ),),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 40.h),
+            child: Column(
+              children: [
+                Text(sub, style: TextStyle(
+                  fontSize: 25.sp,
+                ),),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
